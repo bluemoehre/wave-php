424d1bcce6c27e4d5728ccb31665e3b92f264888
Moehre <blue_moehre@gmx.de>
2016-02-11 00:16:11 +0100

    improved error handling, little changes

--- wave.php
+++ wave.php
@@ -8,13 +8,13 @@
  */
 class Wave
 {
-    const ERR_FILE_NOT_SPECIFIED = 'No file specified.';
-    const ERR_FILE_OPEN = 'Failed to open file.';
-    const ERR_FILE_READ = 'Error while reading from file.';
-    const ERR_FILE_WRITE = 'Error while writing to file.';
-    const ERR_FILE_CLOSE = 'Failed to close file.';
-    const ERR_FILE_INCOMPATIBLE = 'File is not compatible.';
-    const ERR_FILE_HEADER_INVALID = 'File header contains invalid data.';
+    const ERR_PARAM_VALUE = 1;
+    const ERR_FILE_ACCESS = 2;
+    const ERR_FILE_READ = 3;
+    const ERR_FILE_WRITE = 4;
+    const ERR_FILE_CLOSE = 5;
+    const ERR_FILE_INCOMPATIBLE = 6;
+    const ERR_FILE_HEADER = 7;
 
     const SVG_DEFAULT_RESOLUTION_FACTOR = 0.01;
 
@@ -41,12 +41,12 @@ class Wave
     /**
      * @var string
      */
-    protected $subchunk1Id;
+    protected $subChunk1Id;
 
     /**
      * @var integer
      */
-    protected $subchunk1Size;
+    protected $subChunk1Size;
 
     /**
      * @var integer
@@ -81,7 +81,7 @@ class Wave
     /**
      * @var integer
      */
-    protected $subchunk2Size;
+    protected $subChunk2Size;
 
     /**
      * @var integer
@@ -107,9 +107,9 @@ class Wave
     /**
      * @param string $file
      */
-    public function __construct($file)
+    public function __construct($file = '')
     {
-        $this->setFile($file);
+        if (!empty($file)) $this->setFile($file);
     }
 
     /**
@@ -118,93 +118,110 @@ class Wave
      */
     public function setFile($file)
     {
-        if (empty($file)) throw new UnexpectedValueException(self::ERR_FILE_NOT_SPECIFIED);
+        if (empty($file)) throw new UnexpectedValueException('No file specified', self::ERR_PARAM_VALUE);
         $fileHandle = fopen($file, 'r');
-        if ($fileHandle === FALSE) throw new Exception(self::ERR_FILE_OPEN);
+        if ($fileHandle === FALSE) throw new RuntimeException('Failed to open file for reading<', self::ERR_FILE_ACCESS);
         $this->file = $file;
 
         $chunkId = fread($fileHandle, 4);
-        if ($chunkId === FALSE) throw new Exception(self::ERR_FILE_READ);
-        if ($chunkId != "RIFF") throw new Exception(self::ERR_FILE_INCOMPATIBLE);
+        if ($chunkId === FALSE) throw new RuntimeException('Failed to read from file', self::ERR_FILE_READ);
+        if ($chunkId !== 'RIFF') throw new Exception('Unsupported file type', self::ERR_FILE_INCOMPATIBLE);
         $this->chunkId = $chunkId;
 
         $chunkSize = fread($fileHandle, 4);
-        if ($chunkSize === FALSE) throw new Exception(self::ERR_FILE_READ);
+        if ($chunkSize === FALSE) throw new RuntimeException('Failed to read from file', self::ERR_FILE_READ);
         $this->chunkSize = unpack('VchunkSize', $chunkSize);
 
         $format = fread($fileHandle, 4);
-        if ($format != "WAVE") throw new Exception(self::ERR_FILE_INCOMPATIBLE);
+        if ($format === FALSE) throw new RuntimeException('Failed to read from file', self::ERR_FILE_READ);
+        if ($format !== 'WAVE') throw new Exception('Unsupported file format', self::ERR_FILE_INCOMPATIBLE);
         $this->format = $format;
 
         $subChunk1Id = fread($fileHandle, 4);
-        if ($subChunk1Id != "fmt ") throw new Exception(self::ERR_FILE_INCOMPATIBLE);
-        $this->subchunk1Id = $subChunk1Id;
+        if ($subChunk1Id === FALSE) throw new RuntimeException('Failed to read from file', self::ERR_FILE_READ);
+        if ($subChunk1Id !== 'fmt ') throw new Exception('Unsupported file format', self::ERR_FILE_INCOMPATIBLE);
+        $this->subChunk1Id = $subChunk1Id;
 
         $offset = ftell($fileHandle);
         $subChunk1 = fread($fileHandle, 20);
-        $subChunk1 = unpack('Vsubchunk1Size/vaudioFormat/vchannels/VsampleRate/VbyteRate/vblockAlign/vbitsPerSample', $subChunk1);
-        $this->subchunk1Size = $subChunk1['subchunk1Size'];
+        if ($subChunk1 === FALSE) throw new RuntimeException('Failed to read from file', self::ERR_FILE_READ);
+        $subChunk1 = unpack('VsubChunk1Size/vaudioFormat/vchannels/VsampleRate/VbyteRate/vblockAlign/vbitsPerSample', $subChunk1);
+        $this->subChunk1Size = $subChunk1['subChunk1Size'];
         $offset = $offset + 4;
-        if ($subChunk1['audioFormat'] != 1) throw new Exception(self::ERR_FILE_INCOMPATIBLE);
+        if ($subChunk1['audioFormat'] != 1) throw new Exception('Unsupported audio format', self::ERR_FILE_INCOMPATIBLE);
         $this->audioFormat = $subChunk1['audioFormat'];
         $this->channels = $subChunk1['channels'];
         $this->sampleRate = $subChunk1['sampleRate'];
         $this->byteRate = $subChunk1['byteRate'];
         $this->blockAlign = $subChunk1['blockAlign'];
         $this->bitsPerSample = $subChunk1['bitsPerSample'];
-        if ($this->byteRate != $this->sampleRate * $this->channels * $this->bitsPerSample / 8) throw new Exception(self::ERR_FILE_HEADER_INVALID);
-        if ($this->blockAlign != $this->channels * $this->bitsPerSample / 8) throw new Exception(self::ERR_FILE_HEADER_INVALID);
+        if ($this->byteRate != $this->sampleRate * $this->channels * $this->bitsPerSample / 8) throw new Exception('File header contains invalid data', self::ERR_FILE_HEADER);
+        if ($this->blockAlign != $this->channels * $this->bitsPerSample / 8) throw new Exception('File header contains invalid data', self::ERR_FILE_HEADER);
 
-        fseek($fileHandle, $offset + $this->subchunk1Size);
-        if (fread($fileHandle, 4) != 'data') throw new Exception(self::ERR_FILE_HEADER_INVALID);
+        if (fseek($fileHandle, $offset + $this->subChunk1Size) == -1) throw new RuntimeException('Failed to seek in file', self::ERR_FILE_READ);
+        $subChunk2Id = fread($fileHandle, 4);
+        if ($subChunk2Id === FALSE) throw new RuntimeException('Failed to read from file', self::ERR_FILE_READ);
+        if ($subChunk2Id !== 'data') throw new Exception('File header contains invalid data', self::ERR_FILE_HEADER);
 
         $subChunk2 = fread($fileHandle, 4);
+        if ($subChunk2 === FALSE) throw new RuntimeException('Failed to read from file', self::ERR_FILE_READ);
         $subChunk2 = unpack('VdataSize', $subChunk2);
-        $this->subchunk2Size = $subChunk2['dataSize'];
+        $this->subChunk2Size = $subChunk2['dataSize'];
         $this->dataOffset = ftell($fileHandle);
 
         $this->kiloBitPerSecond = $this->byteRate * 8 / 1000;
-        $this->totalSamples = $this->subchunk2Size * 8 / $this->bitsPerSample / $this->channels;
-        $this->totalSeconds = $this->subchunk2Size / $this->byteRate;
+        $this->totalSamples = $this->subChunk2Size * 8 / $this->bitsPerSample / $this->channels;
+        $this->totalSeconds = $this->subChunk2Size / $this->byteRate;
 
-        fclose($fileHandle);
+        if (!fclose($fileHandle)) throw new RuntimeException('Failed to close file', self::ERR_FILE_CLOSE);
     }
 
     /**
      * TODO verify calculations
+     * @param string $outputFile
      * @param float $resolution - Must be <=1. If 1 SVG will be full waveform resolution (amazing large filesize)
-     * @param string $targetFile
      * @return string
      * @throws Exception
      */
-    public function generateSvg($resolution = self::SVG_DEFAULT_RESOLUTION_FACTOR, $targetFile = null)
+    public function generateSvg($outputFile = '', $resolution = self::SVG_DEFAULT_RESOLUTION_FACTOR)
     {
-        $fileHandle = fopen($this->file, 'r');
-        if ($fileHandle === FALSE) throw new Exception(self::ERR_FILE_OPEN);
+        $outputFileHandle = null;
 
-        $samplesPerMerge = $this->sampleRate / ($resolution * $this->sampleRate);
-        $finalSampleVolumes = array();
+        if (!empty($outputFile)) {
+            $outputFileHandle = fopen($outputFile, 'w');
+            if (!$outputFileHandle) throw new RuntimeException('Failed to open output file for writing', self::ERR_FILE_ACCESS);
+        }
+        if (filter_var($resolution, FILTER_VALIDATE_FLOAT) === FALSE) throw new InvalidArgumentException('Resolution must be of type float', self::ERR_PARAM_VALUE);
+        if ($resolution > 1.0 || $resolution < 0.000001) throw new OutOfRangeException('Resolution must be between 1 and 0.000001', self::ERR_PARAM_VALUE);
+
+        if (empty($this->file)) throw new Exception('No file was loaded', self::ERR_FILE_ACCESS);
+        $fileHandle = fopen($this->file, 'r');
+        if (!$fileHandle) throw new RuntimeException('Failed to open file', self::ERR_FILE_ACCESS);
+
+        $sampleSummaryLength = $this->sampleRate / ($resolution * $this->sampleRate);
+        $sampleSummaries = array();
         $i = 0;
-        fseek($fileHandle, $this->dataOffset);
+        if (fseek($fileHandle, $this->dataOffset) == -1) throw new RuntimeException('Failed to seek in file', self::ERR_FILE_READ);
 
         while (($data = fread($fileHandle, $this->bitsPerSample))) {
             $sample = unpack('svol', $data);
             $samples[] = $sample['vol'];
 
-            // when all samples for a block are collected, start calculation for saving memory
-            if ($i > 0 && $i % $samplesPerMerge == 0) {
-                // get average peak within this block
+            // when all samples for a summary are collected, get lows & peaks
+            if ($i > 0 && $i % $sampleSummaryLength == 0) {
                 $minValue = min($samples);
                 $maxValue = max($samples);
-                $finalSampleVolumes[] = array($minValue, $maxValue);
+                $sampleSummaries[] = array($minValue, $maxValue);
                 $samples = array(); // reset
             }
             $i++;
 
-            // TODO analyze side effects
+            // TODO analyze side effects and remove
             // skip to increase speed
-            fseek($fileHandle, $this->bitsPerSample * $this->channels * 3, SEEK_CUR);
+            if (fseek($fileHandle, $this->bitsPerSample * $this->channels * 3, SEEK_CUR) == -1) throw new RuntimeException('Failed to seek in file', self::ERR_FILE_READ);
         }
+
+        if (!fclose($fileHandle)) throw new RuntimeException('Failed to close file', self::ERR_FILE_CLOSE);
 
         $minPossibleValue = pow(2, $this->bitsPerSample) / 2 * -1;
         $maxPossibleValue = $minPossibleValue * -1 - 1;
@@ -212,9 +229,11 @@ class Wave
         $svgPathTop = '';
         $svgPathBottom = '';
 
-        foreach ($finalSampleVolumes as $x => $sampleMinMax) {
+        foreach ($sampleSummaries as $x => $sampleMinMax) {
+            # TODO configurable vertical detail
             $y = round(100 / $range * ($maxPossibleValue - $sampleMinMax[1]));
             $svgPathTop .= "L$x $y";
+            # TODO configurable vertical detail
             $y = round(100 / $range * ($maxPossibleValue + $sampleMinMax[0] * -1));
             $svgPathBottom = "L$x $y" . $svgPathBottom;
         }
@@ -222,7 +241,7 @@ class Wave
         // TODO move gradient to stylesheet
         // TODO this should be improved to use kinda template
         $svg =
-            '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="' . count($finalSampleVolumes) . 'px" height="100px" preserveAspectRatio="none">' .
+            '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="' . count($sampleSummaries) . 'px" height="100px" preserveAspectRatio="none">' .
             '<defs>' .
             '<linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">' .
             '<stop offset="0%" style="stop-color:rgb(0,0,0);stop-opacity:1"/>' .
@@ -233,10 +252,9 @@ class Wave
             '<path d="M0 50' . $svgPathTop . $svgPathBottom . 'L0 50 Z" fill="url(#gradient)"/>' .
             '</svg>';
 
-        if ($targetFile) {
-            if (!$fh = fopen($targetFile, 'w')) throw new Exception(self::ERR_FILE_OPEN);
-            if (!fwrite($fh, $svg)) throw new Exception(self::ERR_FILE_WRITE);
-            if (!fclose($fh)) throw new Exception(self::ERR_FILE_CLOSE);
+        if ($outputFileHandle) {
+            if (fwrite($outputFileHandle, $svg) === FALSE) throw new RuntimeException('Failed to write to output file', self::ERR_FILE_WRITE);
+            if (!fclose($outputFileHandle)) throw new RuntimeException('Failed to close output file', self::ERR_FILE_CLOSE);
         }
 
         return $svg;
